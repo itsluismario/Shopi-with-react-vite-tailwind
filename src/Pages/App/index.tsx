3c698e51acf3d09b5195777dbfa04da4db9744f0
itsluismario <itsluismario@gmail.com>
2023-05-16 09:24:26 -0600

    :building_construction: Routing with react router dom

--- src/Pages/App/index.tsx
+++ src/Pages/App/index.tsx
@@ -1,22 +1,30 @@
+import { BrowserRouter, useRoutes } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
 import Signin from '../Signin'
+import NotFound from '../NotFound'
+
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '', element: <Home/> },
+    { path: '/my-account', element: <MyAccount/> },
+    { path: '/my-order', element: <MyOrder/> },
+    { path: '/my-orders', element: <MyOrders/> },
+    { path: '/sign-in', element: <Signin/> },
+    { path: '/*', element: <NotFound/> },
+  ])
+  return routes
+}
 
+const App = () => {
   return (
-    <h1 className="bg-amber-500">
-      <Home/>
-      <MyAccount/>
-      <MyOrders/>
-      <MyOrder/>
-      <NotFound/>
-      <Signin/>
-    </h1>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   )
 }
 

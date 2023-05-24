6d276061e32885501f4a6780098f224fdbfe3e3b
itsluismario <itsluismario@gmail.com>
2023-05-24 15:30:49 -0400

    :hammer: Navbar optimized

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,97 +1,100 @@
 import { NavLink } from "react-router-dom";
 
+let menuLeft = [
+    {
+        to: '/',
+        text: 'All',
+        className: ''
+    },
+    {
+        to: '/clothes',
+        text: 'clothes',
+        className: ''
+    },
+    {
+        to: '/electronics',
+        text: 'electronics',
+        className: ''
+    },
+    {
+        to: '/furnitures',
+        text: 'furnitures',
+        className: ''
+    },
+    {
+        to: '/toys',
+        text: 'toys',
+        className: ''
+    },
+    {
+        to: '/others',
+        text: 'others',
+        className: ''
+    },
+]
+
+let menuRight = [
+    {
+        to: '/email',
+        text: 'itsluismario@gmail.com',
+        className: 'text-black/60'
+    },
+    {
+        to: '/my-orders',
+        text: 'My orders',
+        className: ''
+    },
+    {
+        to: '/my-account',
+        text: 'My occount',
+        className: ''
+    },
+    {
+        to: '/sign-in',
+        text: 'Sign in',
+        className: ''
+    },
+    {
+        to: '/shoppcar',
+        text: '🛒 (0)',
+        className: ''
+    },
+]
+
 const Navbar = () => {
     const activeStyle = 'underline underline-offset-4'
     return (
         <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-5 text-sm font-light">
             <ul className="flex items-center gap-3">
                 <li className="font-semibold text-lg">
-                    <NavLink 
+                <NavLink 
                         to='/'
                         >
                         Shopi
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/'
-                        className={({ isActive }) => 
-                        isActive ? activeStyle: undefined
-                        }>
-                        All
+                {menuLeft.map(link => (
+                <li className={link.className}>
+                    <NavLink 
+                        to={link.to}
+                        className={({isActive})=> isActive ? activeStyle : undefined }
+                        >
+                        {link.text}
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/clothes'
-                        className={({ isActive }) => 
-                        isActive ? activeStyle: undefined
-                        }>
-                        Clothes
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/electronics'
-                        className={({ isActive }) => 
-                        isActive ? activeStyle: undefined
-                        }>
-                        Electronics
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/furnitures'
-                        className={({ isActive }) => 
-                        isActive ? activeStyle: undefined
-                        }>
-                        Furnitures
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/toys'
-                        className={({ isActive }) => 
-                        isActive ? activeStyle: undefined
-                        }>
-                        Toys
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/others'
-                        className={({ isActive }) => 
-                        isActive ? activeStyle: undefined
-                        }>
-                        Others
-                    </NavLink>
-                </li>
+                ))}
             </ul>
             <ul className="flex items-center gap-3">
-                <li className="text-black/60">
-                    itsluismario@gmail.com
-                </li>
-                <li>
-                    <NavLink to='/my-orders'
-                        className={({ isActive }) => 
-                        isActive ? activeStyle: undefined
-                        }>
-                        My Orders
+                {menuRight.map(link => (
+                <li className={link.className}>
+                    <NavLink 
+                        to={link.to}
+                        className={({isActive})=> isActive ? activeStyle : undefined }
+                        >
+                        {link.text}
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/my-account'
-                        className={({ isActive }) => 
-                        isActive ? activeStyle: undefined
-                        }>
-                        My account
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/sign-in'
-                        className={({ isActive }) => 
-                        isActive ? activeStyle: undefined
-                        }>
-                        Sign In
-                    </NavLink>
-                </li>
-                <li>
-                    🛒 (0)
-                </li>
+                ))}
             </ul>
         </nav>
     ) 

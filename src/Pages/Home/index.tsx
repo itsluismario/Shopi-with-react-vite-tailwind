3b7c145286cb8c9db3b72dbde6c6b82b119396c3
itsluismario <itsluismario@gmail.com>
2023-06-01 14:06:03 -0600

    :sparkles: Fetching API to paint cards

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
+import { apiUrl } from "../../API"
 
 function Home() {
+
+  const [items, setItem] = useState(null)
+    
+  useEffect(() => {
+      fetch(`${apiUrl}/products`)
+      .then(response => response.json())
+      .then(data => setItem(data)
+      )
+  }, [])
+  
   return (
       <Layout>
           Home
-          <Card/>
+          <div className="grid gap-4 grid-cols-4 max-w-screen-lg">
+            {
+              items?.map(item => (
+                <Card key={item.id} data={item} />
+              ))
+            } 
+          </div>
+
       </Layout>
   )
 }
